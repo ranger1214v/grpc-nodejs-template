aef4931196cd31cb609f9873223a8d4b36a8cf53
JeffAiiii <jeff@aiii.ai>
2022-08-22 10:16:25 +0800

    修正調整

--- src/implementations/server/functions/bidiCircleInfoData.ts
+++ src/implementations/server/functions/bidiCircleInfoData.ts
@@ -1,16 +1,31 @@
 import { ServerWritableStream } from "grpc";
 import { CircleInfo } from '../../../protos/action';
-import { circleInfoItem$, circleInfoList$ } from "../../../cache/circleInfoList";
+import { Subject } from "rxjs";
 
+const circleInfos: CircleInfo[]=[];
+const subject = new Subject<CircleInfo>();
 export const BidiCircleInfoData = async (call: ServerWritableStream<CircleInfo>) => {
     let init = false;
     call.on('data', (circleInfo: CircleInfo) => {
         console.log('BidiCircleInfoData =>', circleInfo)
-        circleInfoItem$.next(circleInfo);
+
+        let originalCircleInfo = circleInfos.find(c => c.colorCode == circleInfo.colorCode);
+        if (!originalCircleInfo) {
+            circleInfos.push(circleInfo);
+        }else
+        {
+            originalCircleInfo.x = circleInfo.x;
+            originalCircleInfo.y = circleInfo.y
+        }
+
         if(!init){
             init = true;
             main();
         }
+        
+        subject.next(circleInfo);
+
+
     });
 
     call.on('end', () => {
@@ -18,6 +33,6 @@ export const BidiCircleInfoData = async (call: ServerWritableStream<CircleInfo>)
     });
 
     async function main() {
-        circleInfoList$.subscribe(circleInfo => call.write({ circleInfos: circleInfo }));
+        subject.subscribe(x => call.write({ circleInfos: circleInfos }));
     }
 }
